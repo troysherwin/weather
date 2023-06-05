fc02d3917b3ae0001aa88d1db3e93b86cb140880
troysherwin <119263316+troysherwin@users.noreply.github.com>
2023-06-04 22:05:40 -0500

    added files

---
+++ script.js
@@ -0,0 +1,116 @@
+var x=document.getElementById("demo");
+function getLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+    else{
+        x.innerHTML="Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position){
+    lat=position.coords.latitude;
+    lon=position.coords.longitude;
+    displayLocation(lat,lon);
+}
+
+function showError(error){
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation. Please refresh the page.")
+        break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable. Please refresh the page.")
+        break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out. Please refresh the page.")
+        break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred. Please refresh the page.")
+        break;
+    }
+}
+
+let city2;
+function displayLocation(latitude,longitude){
+    var geocoder;
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(latitude, longitude);
+
+    geocoder.geocode(
+        {'latLng': latlng}, 
+        function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    var add= results[0].formatted_address ;
+                    var  value=add.split(",");
+
+                    count=value.length;
+                    country=value[count-1];
+                    state=value[count-2];
+                    city=value[count-3];
+                    city2 = city;
+                    weather.fetchWeather(city2);
+                }
+                else  {
+                    x.innerHTML = "address not found";
+                }
+            }
+            else {
+                x.innerHTML = "Geocoder failed due to: " + status;
+            }
+        }
+    );
+}
+
+let weather = {
+    apiKey: "0b36b56b33b266722bdcaeb9bd8b9d74",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°F";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " mi/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  
